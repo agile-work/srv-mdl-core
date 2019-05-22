431d44da0043985ec118956980cd33ca16dbf4e9
André Luiz Mendonça <andreluzz@gmail.com>
2019-05-21 21:10:59 -0300

    Instance with permissions

--- services/instance.go
+++ services/instance.go
@@ -6,7 +6,9 @@ import (
 
 	moduleShared "github.com/agile-work/srv-mdl-shared"
 	"github.com/agile-work/srv-mdl-shared/db"
+	"github.com/agile-work/srv-shared"
 	"github.com/agile-work/srv-shared/sql-builder/builder"
+	sql "github.com/agile-work/srv-shared/sql-builder/db"
 	"github.com/go-chi/chi"
 
 	"github.com/agile-work/srv-mdl-core/models"
@@ -26,22 +28,84 @@ func CreateInstance(r *http.Request) *moduleShared.Response {
 
 // LoadAllInstances return all instances from the object
 func LoadAllInstances(r *http.Request) *moduleShared.Response {
-	instances := []models.Instance{}
-	schemaCode := chi.URLParam(r, "schema_code")
+	response := &moduleShared.Response{
+		Code: http.StatusOK,
+	}
 
-	return db.Load(r, &instances, "LoadAllInstances", fmt.Sprintf("%s%s", instancesTablePrefix, schemaCode), nil)
+	schemaCode := chi.URLParam(r, "schema_code")
+	userID := r.Header.Get("userID")
+	languageCode := r.Header.Get("Content-Language")
+
+	// TODO: check user instances permission
+	columns, err := getUserSchemaColumns(schemaCode, userID, languageCode)
+	if err != nil {
+		response.Code = http.StatusInternalServerError
+		response.Errors = append(response.Errors, moduleShared.NewResponseError(shared.ErrorLoadingInstances, "Loading user fields permission", err.Error()))
+		return response
+	}
+
+	instanceTable := fmt.Sprintf("%s%s", instancesTablePrefix, schemaCode)
+	//TODO: Paging and order by
+	statement := builder.Select("id").JSON("data", columns...).From(instanceTable)
+
+	rows, err := sql.Query(statement)
+	if err != nil {
+		response.Code = http.StatusInternalServerError
+		response.Errors = append(response.Errors, moduleShared.NewResponseError(shared.ErrorLoadingInstances, "Loading instances", err.Error()))
+		return response
+	}
+
+	results, err := sql.MapScan(rows)
+	if err != nil {
+		response.Code = http.StatusInternalServerError
+		response.Errors = append(response.Errors, moduleShared.NewResponseError(shared.ErrorLoadingInstances, "Parsing query rows to map", err.Error()))
+		return response
+	}
+
+	response.Data = results
+
+	return response
 }
 
 // LoadInstance return only one object from the database
 func LoadInstance(r *http.Request) *moduleShared.Response {
-	instance := models.Instance{}
+	response := &moduleShared.Response{
+		Code: http.StatusOK,
+	}
+
 	schemaCode := chi.URLParam(r, "schema_code")
 	instanceID := chi.URLParam(r, "instance_id")
-	table := fmt.Sprintf("%s%s", instancesTablePrefix, schemaCode)
-	instanceIDColumn := fmt.Sprintf("%s.id", table)
-	condition := builder.Equal(instanceIDColumn, instanceID)
+	userID := r.Header.Get("userID")
+	languageCode := r.Header.Get("Content-Language")
 
-	return db.Load(r, &instance, "LoadInstance", table, condition)
+	// TODO: check user instances permission
+	columns, err := getUserSchemaColumns(schemaCode, userID, languageCode)
+	if err != nil {
+		response.Code = http.StatusInternalServerError
+		response.Errors = append(response.Errors, moduleShared.NewResponseError(shared.ErrorLoadingInstances, "Loading user fields permission", err.Error()))
+		return response
+	}
+
+	instanceTable := fmt.Sprintf("%s%s", instancesTablePrefix, schemaCode)
+
+	statement := builder.Select("id").JSON("data", columns...).From(instanceTable).Where(builder.Equal("id", instanceID))
+
+	rows, err := sql.Query(statement)
+	if err != nil {
+		response.Code = http.StatusInternalServerError
+		response.Errors = append(response.Errors, moduleShared.NewResponseError(shared.ErrorLoadingInstances, "Loading instances", err.Error()))
+		return response
+	}
+
+	results, err := sql.MapScan(rows)
+	if err != nil {
+		response.Code = http.StatusInternalServerError
+		response.Errors = append(response.Errors, moduleShared.NewResponseError(shared.ErrorLoadingInstances, "Parsing query rows to map", err.Error()))
+		return response
+	}
+
+	response.Data = results[0]
+	return response
 }
 
 // UpdateInstance updates object data in the database
@@ -68,4 +132,22 @@ func DeleteInstance(r *http.Request) *moduleShared.Response {
 	condition := builder.Equal(instanceIDColumn, instanceID)
 
 	return db.Remove(r, "DeleteInstance", table, condition)
+}
+
+func getUserSchemaColumns(schemaCode, userID, languageCode string) ([]string, error) {
+	// TODO: review the need for the language_code
+	statement := builder.Raw("select code from core_v_fields_by_permission where schema_code=$1 and user_id=$2 and language_code=$3", schemaCode, userID, languageCode)
+	rows, err := sql.Query(statement)
+	if err != nil {
+		return nil, err
+	}
+	results, err := sql.MapScan(rows)
+	if err != nil {
+		return nil, err
+	}
+	columns := []string{}
+	for _, result := range results {
+		columns = append(columns, result["code"].(string))
+	}
+	return columns, nil
 }
