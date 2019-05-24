0c7b8cc669dd4f62f5fcaf794b654c66179d8afe
Bruno Piaui <brunopiaui@gmail.com>
2019-05-24 19:04:45 -0300

    Changing relationship from id to code

--- models/tree.go
+++ models/tree.go
@@ -21,7 +21,7 @@ type Tree struct {
 type TreeLevel struct {
 	ID          string    `json:"id" sql:"id" pk:"true"`
 	Code        string    `json:"code" sql:"code"`
-	TreeID      string    `json:"tree_id" sql:"tree_id" fk:"true"`
+	TreeCode    string    `json:"tree_code" sql:"tree_code" fk:"true"`
 	Name        string    `json:"name" table:"core_translations" alias:"core_translations_name" sql:"value" on:"core_translations_name.structure_id = core_tree_levels.id and core_translations_name.structure_field = 'name'"`
 	Description string    `json:"description" table:"core_translations" alias:"core_translations_description" sql:"value" on:"core_translations_description.structure_id = core_tree_levels.id and core_translations_description.structure_field = 'description'"`
 	CreatedBy   string    `json:"created_by" sql:"created_by"`
@@ -34,12 +34,12 @@ type TreeLevel struct {
 type TreeUnit struct {
 	ID              string       `json:"id" sql:"id" pk:"true"`
 	Code            string       `json:"code" sql:"code"`
-	TreeID          string       `json:"tree_id" sql:"tree_id" fk:"true"`
-	ParentID        string       `json:"parent_id" sql:"parent_id" fk:"true"`
+	TreeCode        string       `json:"tree_code" sql:"tree_code" fk:"true"`
+	Path            string       `json:"path" sql:"path"`
 	Name            string       `json:"name" table:"core_translations" alias:"core_translations_name" sql:"value" on:"core_translations_name.structure_id = core_tree_units.id and core_translations_name.structure_field = 'name'"`
 	Description     string       `json:"description" table:"core_translations" alias:"core_translations_description" sql:"value" on:"core_translations_description.structure_id = core_tree_units.id and core_translations_description.structure_field = 'description'"`
 	PermissionScope string       `json:"permission_scope" sql:"permission_scope"`
-	Permissions     []Permission `json:"permissions" sql:"permissions"`
+	Permissions     []Permission `json:"permissions" sql:"permissions" field:"jsonb"`
 	Active          bool         `json:"active" sql:"active"`
 	CreatedBy       string       `json:"created_by" sql:"created_by"`
 	CreatedAt       time.Time    `json:"created_at" sql:"created_at"`
