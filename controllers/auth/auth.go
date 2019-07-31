a7d854608f83c487ada4efc43a94ac613215d6be
Andre Luiz Mendonca <andreluzz@gmail.com>
2019-07-31 12:13:07 -0300

    API Documentation

--- controllers/auth/auth.go
+++ controllers/auth/auth.go
@@ -1,5 +1,10 @@
 package auth
 
+// swagg-doc:controller
+// tags:
+//   - name: Auth
+//     description: Enpoints to deal with authorization
+
 import (
 	"net/http"
 
@@ -7,7 +12,41 @@ import (
 	"github.com/agile-work/srv-mdl-shared/models/user"
 )
 
-// Login endpoint to get user credentials and return token
+// Login swagg-doc:endpoint POST /login
+// summary: Send user credential to get a valid token.
+// tags:
+//   - Auth
+// description: Send user credential to get a valid token.
+// requestBody:
+//   description: User credentials
+//   required: true
+//   content:
+//     application/json:
+//       schema:
+//         type: object
+//         properties:
+//           email:
+//             type: string
+//           password:
+//             type: string
+//         required:
+//           - email
+//           - password
+// responses:
+//   '200':
+//     description: The User data with a valid token
+//     content:
+//       application/json:
+//         schema:
+//           $ref: '#/components/schemas/User'
+//   '400':
+//     description: Invalid request body
+//   '401':
+//     description: Invalid password for this user or the user is not active
+//   '404':
+//     description: Invalid email user not found
+//   '500':
+//     description: Problems related to database connection
 func Login(res http.ResponseWriter, req *http.Request) {
 	user := &user.User{}
 	resp := response.New()
--- main.go
+++ main.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"github.com/agile-work/srv-mdl-core/routes"
-	mdlShared "github.com/agile-work/srv-mdl-shared"
+	shared "github.com/agile-work/srv-mdl-shared"
 )
 
+// swagg-doc:api
+// openapi: 3.0.0
+// info:
+//   title: Horizon API - Core
+//   description: Documentation for the module Core with all endpoits available.
+//   version: 0.0.1
+// servers:
+//   - url: http://localhost:8080/api/v1
+//     description: Development server
+//
+// securitySchemes:
+//   ApiKeyAuth:
+//     type: apiKey
+//     in: header
+//     name: Authentication
+//
+// parameters:
+//   language-code:
+//     name: language-code
+//     in: header
+//     description: Used by translation fields to get the data translatate. Use 'all' to return data from all languages.
+//     required: true
+//     schema:
+//       type: string
+//       example: pt-br
+
 func main() {
-	mdlShared.ListenAndServe("core", "localhost", 3010, installModule, routes.Setup())
+	shared.ListenAndServe("core", "localhost", 3010, installModule, routes.Setup())
 }
 
-func installModule() error {
+func installModule(moduleID string) error {
 	return nil
 }
